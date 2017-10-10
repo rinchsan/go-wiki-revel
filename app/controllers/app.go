1e2ad471aeab8187f55fc1ee7ca4dd4c613fe3f4
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-11 08:38:46 +0900

    Modify saving page data

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -47,13 +47,12 @@ func (c App) edit(title string) revel.Result {
 
 func (c App) save(title string) revel.Result {
 	body := c.Request.FormValue("body")
-	var pages []*models.Page
-	models.DB.Where("title = ?", title).First(&pages)
-	if len(pages) > 0 {
-		p := pages[0]
-		p.Body = []byte(body)
-		p.SaveOrUpdate()
+	p := models.LoadPage(title)
+	if p == nil {
+		p = &models.Page{Title: title}
 	}
+	p.Body = []byte(body)
+	p.SaveOrUpdate()
 	return c.Redirect("/view/" + title)
 }
 
