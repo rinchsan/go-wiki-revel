33cf09f966d7679dbbf4ee8fbc68b5d32b1f740d
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-10-10 18:01:48 +0900

    Update model Page

--- app/models/Page.go
+++ app/models/Page.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"io/ioutil"
+	"regexp"
 )
 
 type Page struct {
@@ -11,14 +12,36 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile("../../public/data"+filename, p.Body, 0600)
+	return ioutil.WriteFile("go-wiki-revel/public/data/"+filename, p.Body, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := "../../public/data" + title + ".txt"
+func LoadPage(title string) (*Page, error) {
+	filename := "go-wiki-revel/public/data/" + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func GetAllPages() ([]*Page, error) {
+	fileInfos, err := ioutil.ReadDir("go-wiki-revel/public/data")
+	if err != nil {
+		return nil, err
+	}
+	var pages []*Page
+	for _, fileInfo := range fileInfos {
+		title := fileInfo.Name()
+		rep, err := regexp.Compile(".txt$")
+		if err != nil {
+			continue
+		}
+		title = rep.ReplaceAllString(title, "")
+		page, err := LoadPage(title)
+		if err != nil {
+			return nil, err
+		}
+		pages = append(pages, page)
+	}
+	return pages, nil
 }
